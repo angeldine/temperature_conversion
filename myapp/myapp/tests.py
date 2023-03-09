b5e305e1f7d7eeef2ef2f30f862667fb844e91c0
BERNADINE AMAKA EMMANUEL <b.emmanuel.22@abdn.ac.uk>
2023-03-09 14:02:25 +0000

    angel

---
+++ myapp/myapp/tests.py
@@ -0,0 +1,27 @@
+from django.test import Client, TestCase
+
+class TemperatureTests(TestCase):
+
+    def test_temp_text(self):
+        client = Client()
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Enter the temperature in fahrenheit")
+    
+    def test_story_text(self):
+        client = Client()
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "which spurs him onto confilct with")
+
+    def test_view_uses_correct_template(self):
+        client = Client()
+        response = client.get('index')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "temp_stories/index.html")
+
+    def test_temp_form(self):
+        client = Client()
+        response = client.post('/', {'temp': 32})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Your converted temperature of 32 in F is 0.0 in C")
